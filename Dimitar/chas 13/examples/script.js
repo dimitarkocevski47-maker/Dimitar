a20799bdfed03dfccc960f810cb8ba4092de517f
dimitarkocevski47-maker <dimitarkocevski47@gmail.com>
2025-12-17 20:51:03 +0100

    JavaScript

---
+++ Dimitar/chas 13/examples/script.js
@@ -0,0 +1,131 @@
+// alert("Hello G1, Welocme to javascript!");
+
+// console.log("Hello G1, Welcome to javascript!");
+
+// declare variable
+let number;
+
+// initialize variable
+number = 5;
+
+// declare and initialize variable
+let number1 = 5;
+
+//  let myNameIsDimitar using camel case
+
+// {let a = 1; local varible}
+
+// string
+
+let str = "This is a string";
+let str2 = "This is also a string";
+let str3 = "2";
+
+// numbers
+
+let num = 1;
+let num1 = 1213421;
+let num2 = -42424;
+
+// boolean
+
+let isValid = true;
+let isValid1 = false;
+
+// undefined
+
+let variable;
+let variable1 = undefined;
+console.log(variable);
+console.log(variable1);
+
+// null
+
+let students = null;
+console.log(students);
+
+let a = "Dimitar";
+let typeOfVariable = typeof a;
+console.log(typeOfVariable);
+
+a = 10;
+console.log(typeof a);
+
+// math operators
+let b = 21;
+let c = 5;
+let sum = b + c;
+console.log(sum);
+
+let subtraction = c - b;
+console.log(subtraction);
+
+let multiply = b * c;
+console.log(multiply);
+
+let divide = b / c;
+console.log(divide);
+
+let modulus = b % c;
+console.log(modulus);
+
+b++;
+console.log(b);
+b = b + 1;
+console.log(b);
+b += 1;
+console.log(b);
+
+b--;
+console.log(b);
+b = b - 1;
+console.log(b);
+b -= 1;
+console.log(b);
+
+//  comparison operators
+
+let first = 10;
+let second = 20;
+console.log(first < second);
+console.log(first <= second);
+
+console.log(first > second);
+console.log(second >= first);
+
+let name = "Dimitar";
+let name1 = "Dragan";
+
+console.log(name == name1);
+console.log(name != name1);
+
+console.log(name == first);
+
+// let feet = 10;
+// let meters = feet * 0.3048;
+// console.log(meters);
+
+let metersPerFeet = 0.3048;
+let feet = 100;
+let meters = feet * metersPerFeet;
+console.log(meters);
+
+// Write a program to calculate the area of a rectangle
+let firstSide = 15;
+let secondSide = 4;
+let area = firstSide * secondSide;
+console.log("The area of the rectangle is: " + area);
+
+// Write a program to calculate the area of a circle
+let pi = Math.PI;
+let r = 10;
+let circleArea = pi * r * r;
+console.log(circleArea);
+
+let circleArea1 = Math.PI * Math.pow(r, 2);
+console.log(circleArea1);
+
+let bcd = 5;
+let aba = "5";
+let str5 = bcd + aba;
+console.log(str5);
