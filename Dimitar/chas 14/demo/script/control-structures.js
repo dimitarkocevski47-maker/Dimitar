9bc16d2c9fa3721883d14168c59a76447d4038a8
dimitarkocevski47-maker <dimitarkocevski47@gmail.com>
2025-12-22 20:59:24 +0100

    JavaScript

---
+++ Dimitar/chas 14/demo/script/control-structures.js
@@ -0,0 +1,53 @@
+let score = 100;
+
+// if (expression) {
+
+// }
+
+// if (score > 100) {
+//   //alert("Score is higher than 100");
+// }
+
+// let score2 = 100;
+
+// if (score2 >= 80) {
+//   console.log("Score is lower than 80");
+// } else {
+//   console.log("Score is higher than 81");
+// }
+
+// if (expression) {
+//     // block of code
+// } else { //WE DO
+// }
+
+// // TEMPLATE
+// if (expression) {
+//     // block of code
+// } else (expression) {
+//     // block of code
+// } else {
+//     // block of code
+// }
+
+let score3 = 100;
+
+if (score3 === 100) {
+  console.log("Score is equal to 100");
+} else if (score3 > 100) {
+  console.log("Score is bigger than 100");
+} else {
+  console.log("score is lower");
+}
+
+let fridayCash = 10;
+
+if (fridayCash >= 50) {
+  console.log("You should go to a dinner and a movie");
+} else if (fridayCash >= 35) {
+  console.log("You should go out to a fine meal");
+} else if (fridayCash >= 12) {
+  console.log("You should go see movie");
+} else {
+  console.log("you should stay hoem");
+}
